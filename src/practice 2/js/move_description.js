2fe4487ffc4add4c0a7a208d5a3e6e1ed8d0fcca
LiChihTseng <lichih.tseng@uqconnect.edu.au>
2023-10-09 01:16:28 +1000

    website implementation folder
     website implementation folder, which include my own 3 html, index, movie_description and movie_lists

---
+++ src/practice 2/js/move_description.js
@@ -0,0 +1,63 @@
+// translate.selectLanguageTag.show = true; //不出现的select的选择语言
+// translate.execute();
+
+
+const baseURL = "https://damp-castle-86239-1b70ee448fbd.herokuapp.com/decoapi/movies/";
+
+// 从URL参数中获取电影标识符
+const urlParams = new URLSearchParams(window.location.search);
+const movieIdentifier = urlParams.get("movie");
+
+// 如果有电影标识符，则调用API以获取该电影的详细信息
+if (movieIdentifier) {
+    // 构建电影详细信息的URL，例如：
+    const movieDetailURL = `${baseURL}?title=${movieIdentifier}`;
+
+    // 发起API请求以获取电影详细信息
+    fetch(movieDetailURL)
+        .then(response => response.json())
+        .then(data => {
+            // 在此处使用API响应中的数据来填充页面元素，显示电影详细信息
+            const movieDetails = data[0];
+            if (movieDetails) {
+                document.getElementById("movie-title").textContent = movieDetails.title;
+                // document.getElementById("release-date").textContent = `Release Date: ${movieDetails.release_date}`;
+                document.getElementById("category").textContent = `Category: ${movieDetails.category}`;
+                document.getElementById("movie-img").src = movieDetails.image_url;
+                document.getElementById("small-movie-img").src = movieDetails.image_url;
+
+                
+
+                // Create an empty array to store cinema details
+                const cinemaDetails = [];
+
+                movieDetails.cinema_details.forEach(cinema => {
+                    const cinemaDetail = `${cinema.cinema_name}: $${cinema.ticket_price}`;
+                    cinemaDetails.push(cinemaDetail);
+                });
+                
+                // Set the text content of the element with id "ticket_price" to the cinema details
+                document.getElementById("ticket-price").innerHTML  = cinemaDetails.join("<br>");
+                
+            } else {
+                // 处理电影不存在的情况
+                document.getElementById("movie-title").textContent = "Movie Not Found";
+            }
+        })
+        .catch(error => {
+            console.error("Error fetching movie details:", error);
+        });
+} else {
+    // 如果没有电影标识符，可能显示错误消息或默认内容
+    document.getElementById("movie-title").textContent = "Movie Not Specified";
+}
+
+let buttons = document.querySelector('.buttons');
+let btn = buttons.querySelectorAll('.font-size-btn');
+for (var i = 0; i <btn.length; i++){
+    btn[i].addEventListener('click', function(){
+        let current = document.getElementsByClassName('active');
+        current[0].className = current[0].className.replace("active","");
+        this.className += " active";
+    })
+}
---
+++ src/website_implementation/js/index.js
@@ -0,0 +1,103 @@
+
+
+const buttons = document.querySelectorAll("[data-carousel-button]")
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const offset = button.dataset.carouselButton === "next" ? 1 : -1
+        const slides = button
+            .closest("[data-carousel]")
+            .querySelector("[data-slides]")
+
+        const activeSlide = slides.querySelector("[data-active]")
+        let newIndex = [...slides.children].indexOf(activeSlide) + offset
+
+        if (newIndex < 0) newIndex = slides.children.length - 1
+        if (newIndex >= slides.children.length) newIndex = 0
+
+        slides.children[newIndex].dataset.active = true
+        delete activeSlide.dataset.active
+    })
+})
+
+
+// API base URL
+const baseURL = "https://damp-castle-86239-1b70ee448fbd.herokuapp.com/decoapi/movies/";
+
+// Fetch all movies
+fetch(baseURL)
+    .then(response => response.json())
+    .then(data => {
+        // Get the container to display movie details
+        const container = document.getElementById("card-container");
+
+        // Loop through the movie data and create HTML elements
+        data.forEach((movie, index) => {
+            // Create a movie details div
+            const movieDetailsDiv = document.createElement("div");
+            movieDetailsDiv.classList.add("card");
+
+            // Set tabindex dynamically based on the index
+            movieDetailsDiv.tabIndex = index + 1;
+
+            // 為卡片設定一個唯一的識別符號（例如，電影標題）
+            const cardIdentifier = movie.title;
+
+            // 新增一個點擊事件監聽器到卡片上
+            movieDetailsDiv.addEventListener("click", () => {
+                // 根據識別符號構建新頁面的 URL，包含 `movie_description.html` 和識別符號
+                const newPageURL = `movie_description.html?movie=${cardIdentifier}`;
+
+                // 程式方式導向到新頁面
+                window.location.href = newPageURL;
+            });
+            // Create and set the movie title
+            const title = document.createElement("h2");
+            title.setAttribute('id', 'card-title')
+            title.textContent = movie.title;
+
+            // Create and set the movie release date
+            const releaseDate = document.createElement("p");
+            releaseDate.setAttribute('id','release-date');
+            releaseDate.textContent = `2023`;
+
+            // Create and set the movie category
+            const category = document.createElement("p");
+            category.setAttribute('id', 'category')
+            category.textContent = `Category: ${movie.category}`;
+
+            // Create an image element for the movie poster
+            const posterImage = document.createElement("img");
+            posterImage.src = movie.image_url; // Set the image source
+
+            // Create a cinema details div
+            const cinemaDetailsDiv = document.createElement("div");
+            cinemaDetailsDiv.classList.add("cinema-details");
+
+            // Loop through cinema details
+            movie.cinema_details.forEach(cinema => {
+                const cinemaDetail = document.createElement("p");
+                // cinemaDetail.textContent = `Cinema: ${cinema.cinema_name}, Session Time: ${cinema.session_time}, Ticket Price: $${cinema.ticket_price}`;
+                cinemaDetail.textContent = `${cinema.cinema_name}: $${cinema.ticket_price}`;
+                cinemaDetailsDiv.appendChild(cinemaDetail);
+            });
+
+            // Append all elements to the movie details div
+
+            movieDetailsDiv.appendChild(posterImage); // Append the image
+            movieDetailsDiv.appendChild(releaseDate);
+            movieDetailsDiv.appendChild(title);
+
+            movieDetailsDiv.appendChild(category);
+            movieDetailsDiv.appendChild(cinemaDetailsDiv);
+
+            // Append the movie details div to the container
+            container.appendChild(movieDetailsDiv);
+        });
+    })
+    .catch(error => {
+        console.error("Error fetching movie data:", error);
+    });
+
+// Call the function to fetch and display movie details
+fetchMovieDetails();
